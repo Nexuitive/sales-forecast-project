2ad320ef989378082fe351d2c9e5962a34720040
Osama <nexuitive@gmail.com>
2026-05-02 16:11:15 +0500

    Updated dashboard with insights

--- app.py
+++ app.py
@@ -48,12 +48,37 @@ def upload_file():
     result = forecast[['ds', 'yhat']].tail(30)
 
     # Graph
-    fig = px.line(
-        forecast,
-        x='ds',
-        y='yhat',
-        title='Future Sales Forecast'
-    )
+    fig = px.line(title="Sales Forecast (Actual vs Predicted)")
+
+    # Actual Data
+    fig.add_scatter(
+    x=df['ds'],
+    y=df['y'],
+    mode='lines',
+    name='Actual Sales')
+
+    # Predicted Data
+    fig.add_scatter(
+    x=forecast['ds'],
+    y=forecast['yhat'],
+    mode='lines',
+    name='Predicted Sales')
+
+    # Upper Bound (Best Case)
+    fig.add_scatter(
+    x=forecast['ds'],
+    y=forecast['yhat_upper'],
+    mode='lines',
+    name='Upper Bound',
+    line=dict(dash='dot'))
+
+    # Lower Bound (Worst Case)
+    fig.add_scatter(
+    x=forecast['ds'],
+    y=forecast['yhat_lower'],
+    mode='lines',
+    name='Lower Bound',
+    line=dict(dash='dot'))
 
     graph_html = fig.to_html(full_html=False)
 
@@ -61,6 +86,21 @@ def upload_file():
     avg_sales = round(result['yhat'].mean(),2)
     max_sales = round(result['yhat'].max(),2)
     min_sales = round(result['yhat'].min(),2)
+    
+    # Trend Insight
+    if result['yhat'].iloc[-1] > result['yhat'].iloc[0]:
+    trend = "Sales are expected to increase in the coming period."
+    else:
+    trend = "Sales are expected to decrease in the coming period."
+
+    # Demand Insight
+    if avg_sales > df['y'].mean():
+    demand = "Higher demand is predicted compared to past sales."
+    else:
+    demand = "Demand is expected to remain stable or slightly lower."
+
+    # Final Insight
+    insight = trend + " " + demand
 
     # Table HTML
     table_html = result.to_html(index=False)
@@ -72,6 +112,7 @@ def upload_file():
         avg_sales=avg_sales,
         max_sales=max_sales,
         min_sales=min_sales
+        insight=insight
     )
 
 
